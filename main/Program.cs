2e3506ebc8a280d554747b9c4ce356b50db917ab
it.iosdev7 <it.iosdev7@dawateislami.net>
2022-08-24 03:19:29 +0500

    Add File and Read

---
+++ main/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Program
+{
+    class Program
+    {
+        static readonly string textFile = "assets/sample.txt"; 
+        static void Main(String[] args){
+            Console.WriteLine("Hello ");
+            
+             string text = File.ReadAllText(textFile);  
+             Console.WriteLine(text);  
+            
+        }
+    
+ 
+    }
+
+}
