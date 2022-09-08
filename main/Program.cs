d92a0be64755ad76c11b2c818cddf567ef693dc1
it.iosdev7 <it.iosdev7@dawateislami.net>
2022-09-08 21:10:49 +0500

    complete syntax analyzer

--- main/Program.cs
+++ main/Program.cs
@@ -8,6 +8,7 @@ namespace Program
     class Program
     {
         static readonly string textFile = "assets/sample.txt";
+        static readonly string outPutFile = "assets/farooq.txt";
         static int lineNumber = 1;
 
 
@@ -592,10 +593,14 @@ namespace Program
             Program program = new Program(); // Creating Object  
             Dictionary<int, Tuple<int, string>> words = program.breakWord(ref text); // Calling Function    
             Dictionary<int, Tuple<int, string, string>> classes = program.classPart(ref words);
+            StreamWriter sw = new StreamWriter(outPutFile);
             foreach (var item in classes)
             {
+                sw.WriteLine( item.Key + "::"+ item.Value);
                 Console.Write(item.Key + "::"); Console.WriteLine(item.Value);
+
             }
+            sw.Close();
         }
     }
 }
