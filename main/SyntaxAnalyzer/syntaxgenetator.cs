e67ae16ff0ae822b7ae7bad8c2a294e85fd17e02
it.iosdev7 <it.iosdev7@dawateislami.net>
2022-10-16 17:46:51 +0500

    while loop

--- main/SyntaxAnalyzer/syntaxgenetator.cs
+++ main/SyntaxAnalyzer/syntaxgenetator.cs
@@ -1,9 +1,9 @@
 class SyntaxGenerator{
     
-    string[] whileSelectionSet = {"while"};
+    String[] whileSelectionSet = {"while"};
 
-    public void starting(ref string text){
-        Console.WriteLine(text);
+    public void starting(ref Dictionary<int, Tuple<int, string, string>>  text){
+        recognizeCFG(ref text, whileSelectionSet.ToList());
 
     }
 
@@ -11,7 +11,12 @@ class SyntaxGenerator{
 
     }
 
-    void recognizeCFG(List<String> listofSelectionsSet ){
-
+    void recognizeCFG(ref Dictionary<int, Tuple<int, string, string>>  text ,List<string> listofSelectionsSet){
+        for (int i = 1; i < text.Count ; i++){
+            if (text[i].Item3 == listofSelectionsSet[0])
+            {
+               Console.WriteLine("Yes");
+            }
+        }
     }
 }
